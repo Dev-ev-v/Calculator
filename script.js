5496a78129b316c1a2a10643bb79a5c00cd6c2ac
Dev-ev-v <jogos.testedostestes@gmail.com>
2025-08-07 22:31:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+const visor = document:getElementById("visor");
+function calculate(x) {
+visor.value += x;
+}
+function clear() {
+  visor.value = "";
+}
+function finish() {
+visor.value = eval(visor.value);
+}
